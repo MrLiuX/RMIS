90abc994a6e2eed2a3d71a6825fa798078c488b9
Mrliux <mrliuxchn@gmail.com>
2020-07-11 02:38:19 +0800

    更新SQL脚本

--- sql/RMIS.sql
+++ sql/RMIS.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50729
  File Encoding         : 65001
 
- Date: 05/07/2020 21:17:03
+ Date: 11/07/2020 02:36:13
 */
 
 SET NAMES utf8mb4;
@@ -181,16 +181,17 @@ INSERT INTO `损坏单项扣分表` VALUES ('沥青', '龟裂', 100.00, 80);
 -- ----------------------------
 DROP TABLE IF EXISTS `损坏扣分表`;
 CREATE TABLE `损坏扣分表`  (
-  `记录编号` int(6) NOT NULL,
+  `检查编号` int(6) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT,
   `路面类型` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `损坏类型` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `损坏面积` decimal(10, 5) NOT NULL,
   `损坏密度` decimal(10, 5) NOT NULL,
   `单项扣分值` decimal(10, 5) NOT NULL,
-  PRIMARY KEY (`记录编号`) USING BTREE,
   INDEX `路面类型1`(`路面类型`) USING BTREE,
-  INDEX `损坏类型02`(`损坏类型`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+  INDEX `损坏类型02`(`损坏类型`) USING BTREE,
+  INDEX `3`(`检查编号`) USING BTREE,
+  CONSTRAINT `3` FOREIGN KEY (`检查编号`) REFERENCES `道路设施路面损害情况` (`检查编号`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 23 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of 损坏扣分表
@@ -242,14 +243,13 @@ CREATE TABLE `损坏详情`  (
   `损坏长` decimal(10, 5) NOT NULL,
   `损坏宽` decimal(10, 5) NOT NULL,
   `损坏高` decimal(10, 5) NOT NULL,
-  `损坏面积` decimal(10, 5) NOT NULL,
+  `损坏面积` decimal(10, 5) NULL DEFAULT NULL,
   `损坏位置及损坏情况描述` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `备注` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  INDEX `检查编号1`(`检查编号`) USING BTREE,
   INDEX `损坏类型2`(`损坏类型`) USING BTREE,
-  CONSTRAINT `损坏类型2` FOREIGN KEY (`损坏类型`) REFERENCES `损坏类型` (`损坏类型`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `检查编号1` FOREIGN KEY (`检查编号`) REFERENCES `道路设施路面损害情况` (`检查编号`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 19 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+  INDEX `1`(`检查编号`) USING BTREE,
+  CONSTRAINT `1` FOREIGN KEY (`检查编号`) REFERENCES `道路设施路面损害情况` (`检查编号`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 23 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of 损坏详情
@@ -272,15 +272,13 @@ CREATE TABLE `系统人员`  (
 -- ----------------------------
 -- Records of 系统人员
 -- ----------------------------
-INSERT INTO `系统人员` VALUES ('123', '123', '刘旭', '管理员');
-INSERT INTO `系统人员` VALUES ('shy', '123', '豆玉明', '管理员');
 
 -- ----------------------------
 -- Table structure for 设施损害通知单
 -- ----------------------------
 DROP TABLE IF EXISTS `设施损害通知单`;
 CREATE TABLE `设施损害通知单`  (
-  `通知号` int(6) NOT NULL,
+  `通知号` int(6) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT,
   `日期` date NOT NULL,
   `设施名称` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `损坏部位` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
@@ -288,7 +286,7 @@ CREATE TABLE `设施损害通知单`  (
   `处理意见` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `技术员` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`通知号`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of 设施损害通知单
@@ -487,7 +485,7 @@ CREATE TABLE `道路设施日常巡查情况记录`  (
   INDEX `巡检损害类型`(`损坏类型`) USING BTREE,
   CONSTRAINT `巡检损害类型` FOREIGN KEY (`损坏类型`) REFERENCES `损坏类型` (`损坏类型`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `道路巡查` FOREIGN KEY (`道路编号`) REFERENCES `道路关键信息` (`道路编号`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 22 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 25 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of 道路设施日常巡查情况记录
@@ -508,7 +506,7 @@ CREATE TABLE `道路设施路面损害情况`  (
   PRIMARY KEY (`检查编号`) USING BTREE,
   INDEX `道路编号2`(`道路编号`) USING BTREE,
   CONSTRAINT `道路编号2` FOREIGN KEY (`道路编号`) REFERENCES `道路关键信息` (`道路编号`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 20 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 25 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of 道路设施路面损害情况
@@ -528,7 +526,7 @@ CREATE TABLE `道路路面平整度检测`  (
   PRIMARY KEY (`检测编号`) USING BTREE,
   INDEX `平整度检测道路编号`(`道路编号`) USING BTREE,
   CONSTRAINT `平整度检测道路编号` FOREIGN KEY (`道路编号`) REFERENCES `道路关键信息` (`道路编号`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of 道路路面平整度检测
